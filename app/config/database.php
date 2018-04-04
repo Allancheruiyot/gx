354fdc9c5056818305451d0211281cd513e31d85
kenkode <wangoken2@gmail.com>
2018-04-04 15:52:20 +0300

    db

--- app/config/database.php
+++ app/config/database.php
@@ -58,7 +58,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'gx',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'mysql',
 			'port' => '3306',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
